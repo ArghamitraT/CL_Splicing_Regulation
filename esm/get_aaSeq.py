82f66187311fab9f8fdc959dbf93475248aba1bf
Nicholas Keung <nlkeung@yahoo.com>
2025-04-21 00:04:04 -0400

    Finished script to extract info from AA alignment into CSV

--- esm/get_aaSeq.py
+++ esm/get_aaSeq.py
@@ -2,7 +2,7 @@
 Searches for a given gene and its amino acid alignment with other species.
 """
 
-# import pandas as pd
+import pandas as pd
 # import gzip
 import re
 import pickle
@@ -16,20 +16,52 @@ input_file = "/gpfs/commons/home/nkeung/data/" + alignment_file
 # [+-]? + or -, (optional)
 # Ex. ">ENST00000350908.9_panTro4_1_16 56 0 0 chr7:115900699-115900866+"
 
-pattern = re.compile(r"^>(\S+) (\d+) (\d+) (\d+) (\S+):(\d+)-(\d)[+-]?")
+pattern = re.compile(r"^>(\S+) (\d+) (\d+) (\d+) (\S+):(\d+)-(\d+)([+-]?)")
 
 enst_id = "ENST00000350908.9"
-species = ["hg38"]
-seq_length = "56"       # corresponds to match.group(2)
+search_species = ["hg38", "mm10", "panTro4", "ponAbe2", "gorGor3", "rheMac3"]
 
-found_Match = False
+# To store in CSV
+data = []
+
 with open(input_file, "r") as file:
     lines = file.readlines()
 
-for header, sequence in zip(lines[::2], lines[1::2]):
+for header, sequence in zip(lines[::2], lines[1::2]):               # zip pairs of lines together
         match = pattern.match(header)
-        if enst_id in header:
-            if any(sp in header for sp in species):
-                # if match.group(2) == seq_length:
-                print(sequence)
-        
+        if enst_id in header:                                       # match Ensembl transcript ID
+            found_species = None
+            for sp in search_species:                               # iterate through the species we're looking for
+                 if sp in header:
+                      found_species = sp
+            if found_species and match:
+                # Store protein sequence information
+                identifier = match.group(1)
+                length_aa = match.group(2)
+                start_phase = match.group(3)
+                end_phase = match.group(4)
+                chromosome = match.group(5)
+                start_coord = match.group(6)
+                end_coord = match.group(7)
+                strand = match.group(8)
+                aa_sequence = sequence
+
+                # Get species identifier
+                parts = identifier.split('_')
+                species_name = parts[1]     # e.g. hg38 or panTro4
+                exon_num = parts[2]        # e.g. _species_1_16
+
+                # Add to data
+                data.append((species_name, exon_num, length_aa, start_phase, end_phase, chromosome, start_coord, 
+                             end_coord, strand, aa_sequence))
+
+df = pd.DataFrame(data, columns = [
+     'Species Name', 'Exon Number', 'AA Length', 'Start Phase', 'End Phase', 'Chromosome', 'Start Coord', 
+     'End Coord', 'Strand', 'Sequence'
+])
+
+file_name = "foxp2-aa-sequences"
+full_name = f'/gpfs/commons/home/nkeung/data/processed_data/{file_name}'
+df.to_csv(full_name+".csv", index=False)
+
+print("Successfully saved amino acid sequences")
