c287868b6cdfee95dc0bab493f218497f6677e03
Nicholas Keung <nlkeung@yahoo.com>
2025-08-14 12:48:18 -0400

    Test embedder, optimizer, and loss combinations

--- ML_model/scripts/run_cl_training.sh
+++ ML_model/scripts/run_cl_training.sh
@@ -3,8 +3,8 @@
 
 # === Set main data directory once ===
 
-# MAIN_DIR="/gpfs/commons/home/atalukder/Contrastive_Learning/data/final_data/intronExonSeq_multizAlignment_noDash/trainTestVal_data"
-MAIN_DIR="/mnt/home/nlk2136/Contrastive_Learning/data/final_data/intronExonSeq_multizAlignment_noDash/trainTestVal_data"
+MAIN_DIR="/gpfs/commons/home/atalukder/Contrastive_Learning/data/final_data/intronExonSeq_multizAlignment_noDash/trainTestVal_data"
+# MAIN_DIR="/mnt/home/nlk2136/Contrastive_Learning/data/final_data/intronExonSeq_multizAlignment_noDash/trainTestVal_data"
 
 
 # === Just specify file names ===
@@ -23,11 +23,7 @@ export TEST_DATA_FILE="${MAIN_DIR}/${TEST_FILE}"
 
 # export CUDA_VISIBLE_DEVICES=1
 
-<<<<<<< HEAD
-NOTES="empire ai terminal test run"
-=======
-NOTES="termianl test run"
->>>>>>> f079f4b (Modify script for run on NYGC)
+NOTES="resnet101 supcon 2 aug sgd"
 
 python -m scripts.cl_training \
         task=introns_cl \
@@ -38,11 +34,11 @@ python -m scripts.cl_training \
         trainer.max_epochs=2 \
         trainer.val_check_interval=1.0\
         optimizer="sgd" \
-        trainer.devices=1\
-        logger.name="test_run$(date +%Y%m%d_%H%M%S)"\
+        trainer.devices=3\
+        logger.name="resnet101_supcon_sgd_2_$(date +%Y%m%d_%H%M%S)"\
         embedder.maxpooling=True\
         logger.notes="$NOTES"\
-        dataset.n_augmentations=10 \
+        dataset.n_augmentations=2 \
         dataset.train_data_file=$TRAIN_DATA_FILE \
         dataset.val_data_file=$VAL_DATA_FILE \
         dataset.test_data_file=$TEST_DATA_FILE
