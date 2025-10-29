850c663a512c51ce5605bf1ea4b44ee0b99a5e44
Nicholas Keung <nlkeung@yahoo.com>
2025-10-29 00:03:14 -0400

    Adjust time requested by number of cells

--- Tabula_sapiens/submit_jobs.py
+++ Tabula_sapiens/submit_jobs.py
@@ -8,12 +8,13 @@ import json
 
 # -------------------- CONFIG --------------------
 
-hour = 2
+hour = 2        # Modified in main. See below
 memory = 128
 nthred = 8
 
 data_dir = "/gpfs/commons/home/nkeung/tabula_sapiens"
 metadata_file = os.path.join(data_dir, "bam_paths.tsv")
+cell_counts_file = os.path.join(data_dir, "cell_counts.tsv")
 slurm_dir = os.path.join(data_dir, "jobs")
 
 code_dir = "/gpfs/commons/home/nkeung/Contrastive_Learning/code/Tabula_sapiens"
@@ -42,6 +43,7 @@ def create_prg(cell_type):
     WORKDIR={code_dir}
     cd $WORKDIR
     python compile_txt.py --cell_type "{cell_type}" --metadata {metadata_file} --main_dir {data_dir}
+    conda deactivate
 
     # Run rMATS
     source $HOME/miniconda3/etc/profile.d/conda.sh
@@ -69,16 +71,18 @@ def create_prg(cell_type):
 
 def create_slurm_file(prg_script, slurm_file_path, cell_type):
     safe_cell = cell_type.replace(" ", "_")
+    timestamp = time.strftime("_%Y%m%d_%H%M%S")
+    job_name = f"ts_{safe_cell}{timestamp}"
     header = f"#!/bin/bash\n" + \
     "##ENVIRONMENT SETTINGS; REPLACE WITH CAUTION\n" + \
     "##NECESSARY JOB SPECIFICATIONS\n" + \
-    f"#SBATCH --job-name=ts_{safe_cell}      #Set the job name to \"JobExample1\"\n" + \
+    f"#SBATCH --job-name=ts_{job_name}      #Set the job name to \"JobExample1\"\n" + \
     "#SBATCH --partition=cpu     \n" + \
     f"#SBATCH --time={hour}:00:00              #Set the wall clock limit \n" + \
     f"#SBATCH --mem={memory}G              \n" + \
     f"#SBATCH --cpus-per-task={nthred}                   \n" + \
     "#SBATCH --mail-type=END,FAIL    \n" + \
-    f"#SBATCH --output={slurm_dir}/output/out_{safe_cell}.%j      #Send stdout/err to\n" + \
+    f"#SBATCH --output={slurm_dir}/output/out_{job_name}.%j      #Send stdout/err to\n" + \
     "#SBATCH --mail-user=nkeung@nygenome.org                    \n\n" + \
     f"{prg_script}"
 
@@ -105,7 +109,15 @@ def submit_job(cell):
 def main():
     ts = pd.read_csv(metadata_file, sep="\t")
     cells = ts["cell_type"].unique().tolist()
+
+    counts = pd.read_csv(cell_counts_file, sep="\t")
     for cell in cells:
+        minutes_per_cell = 54 / 569  # runtime per cell in minutes
+        num_cells = counts.loc[counts['cell_type'] == cell, 'count'].iloc[0]
+        estimated_minutes = int(num_cells * minutes_per_cell * 1.2)  # add 20% buffer
+        global hour
+        hour = max(1, estimated_minutes // 60 + 1)  # round up to hours, min 1h
+
         safe_cell = cell.replace(" ", "_")
         if safe_cell in completed_cells:
             continue
