fbe4a0d5ac490e80b0066020c93d0e15e49c121b
Nicholas Keung <nlkeung@yahoo.com>
2025-11-03 17:23:47 -0500

    Added mean calculation checks and generate plots for psi distribution across splits

--- Tabula_sapiens/data_validation.py
+++ Tabula_sapiens/data_validation.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import numpy as np
 import json
+from scipy.special import logit
+import matplotlib.pyplot as plt
+import os
 
 def sanity_checks(full_df, psi_cols):
     problems = 0
@@ -41,7 +44,8 @@ def sanity_checks(full_df, psi_cols):
     if len(psi_cols) > 0:
         print("✅ Checked PSI columns for valid values")
 
-    # 5. All rows must have at least one PSI value
+
+    # 5.1 All rows must have at least one PSI value
     psi_numeric = full_df[psi_cols].apply(pd.to_numeric, errors='coerce')
 
     # Mask: True if row has **all NaN** in numeric form (i.e., no numeric PSI at all)
@@ -57,7 +61,7 @@ def sanity_checks(full_df, psi_cols):
         print(f"✅ All rows have at least one numeric PSI value")
 
 
-    # # 5.5 For training/validation/test sets, no NAN values allowed. Must be -1
+    # # 5.2 For CSVs with no mean, no NAN values allowed. Must be -1
     # psi_numeric = full_df[psi_cols].apply(pd.to_numeric, errors='coerce')
 
     # # Mask: All rows where PSI is not NA and non-negative OR PSI is -1
@@ -81,6 +85,35 @@ def sanity_checks(full_df, psi_cols):
             problems += 1
         else:
             print(f"✅ Column {col} has no NaN values")
+    
+
+    # 7 Ensure PSI is the true average of the row
+    psi_numeric = full_df[psi_cols].apply(pd.to_numeric, errors='coerce')
+    computed_mean = psi_numeric.mean(axis=1, skipna=True)
+
+    mismatches = full_df[
+        (full_df["mean_psi"] - computed_mean).abs() > 1e-6
+    ]
+
+    if len(mismatches) > 0:
+        print(f"❌ Mean PSI does not match in {len(mismatches)} rows")
+        problems += 1
+    else:
+        print(f"✅ Mean PSI matches in all rows")
+
+
+    # 8. Ensure logit mean PSI also matches
+    eps = 1e-6
+    computed_logit = logit(np.clip(computed_mean / 100, eps, 1-eps))
+    mismatch_logit = full_df[
+        (full_df["logit_mean_psi"] - computed_logit).abs() > eps
+    ]
+    if len(mismatch_logit) > 0:
+        print(f"❌ Logit mean PSI does not match in {len(mismatch_logit)} rows")
+        problems += 1
+    else:
+        print(f"✅ Logit mean PSI matches in all rows")
+
 
     if problems == 0:
         print(f"\n✅ All checks passed!")
@@ -89,18 +122,62 @@ def sanity_checks(full_df, psi_cols):
 
 
 
-def main():
-    full_ascot_file = "/gpfs/commons/home/nkeung/tabula_sapiens/psi_data/final_data/full_cassette_exons_with_mean_psi.csv"
-    # full_ascot_file = "/gpfs/commons/home/nkeung/tabula_sapiens/psi_data/final_data/val_cassette_exons.csv"
+def generate_plots(train_df, val_df, test_df, output_dir):
+    plt.figure(figsize=(8,5))
+    plt.hist(train_df["mean_psi"], bins=50, alpha=0.5, label="Train")
+    plt.hist(val_df["mean_psi"], bins=50, alpha=0.5, label="Val")
+    plt.hist(test_df["mean_psi"], bins=50, alpha=0.5, label="Test")
+    plt.xlabel("Mean PSI (%)")
+    plt.ylabel("Frequency")
+    plt.legend()
+    plt.title("PSI distribution across splits")
+    plt.savefig(os.path.join(output_dir, "mean_psi_dist.png"), dpi=300, bbox_inches="tight")
 
-    full_df = pd.read_csv(full_ascot_file)
+    plt.figure(figsize=(8,5))
+    plt.hist(train_df["logit_mean_psi"], bins=50, alpha=0.5, label="Train")
+    plt.hist(val_df["logit_mean_psi"], bins=50, alpha=0.5, label="Val")
+    plt.hist(test_df["logit_mean_psi"], bins=50, alpha=0.5, label="Test")
+    plt.xlabel("Logit Mean PSI (%)")
+    plt.ylabel("Frequency")
+    plt.legend()
+    plt.title("Logit PSI distribution across splits")
+    plt.savefig(os.path.join(output_dir, "logit_mean_psi_dist.png"), dpi=300, bbox_inches="tight")
+
+
+def main():
+    # full_ascot_file = "/gpfs/commons/home/nkeung/tabula_sapiens/psi_data/final_data/full_cassette_exons_with_mean_psi.csv"
+    final_train_data = "/gpfs/commons/home/atalukder/Contrastive_Learning/data/final_data/TSCelltype_finetuning/train_cassette_exons_with_logit_mean_psi.csv"
+    final_validation_data = "/gpfs/commons/home/atalukder/Contrastive_Learning/data/final_data/TSCelltype_finetuning/val_cassette_exons_with_logit_mean_psi.csv"
+    final_test_data = "/gpfs/commons/home/atalukder/Contrastive_Learning/data/final_data/TSCelltype_finetuning/test_cassette_exons_with_logit_mean_psi.csv"
+
+    train_df = pd.read_csv(final_train_data)
+    val_df = pd.read_csv(final_validation_data)
+    test_df = pd.read_csv(final_test_data)
 
     with open("/gpfs/commons/home/nkeung/tabula_sapiens/completed.json", "r") as f:
         cells = list(json.load(f))
         psi_cols = [x.replace("_", " ") for x in cells]
     # psi_cols = ["pericyte", "mesenchymal stem cell of adipose tissue", "ltf+ epithelial cell"]    # For subset exons
 
-    sanity_checks(full_df, psi_cols)
+    for name, df in [("Train", train_df), ("Val", val_df), ("Test", test_df)]:
+        print(f"\n{name} dtypes:")
+        print(df.dtypes)
+
+    
+    print(f"===== Training Data =====")
+    sanity_checks(train_df, psi_cols)
+    print()
+
+    print(f"===== Validation Data =====")
+    sanity_checks(val_df, psi_cols)
+    print()
+
+    print(f"===== Test Data =====")
+    sanity_checks(test_df, psi_cols)
+    print()
+
+    output_dir = "/gpfs/commons/home/nkeung/tabula_sapiens/psi_data/final_data/figures"
+    generate_plots(train_df, val_df, test_df, output_dir)
     
 if __name__ == "__main__":
     main()
