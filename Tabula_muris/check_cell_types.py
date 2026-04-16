ac385223a2096c88c4bddadc5c469be5c45bf21f
Nicholas Keung <nlkeung@yahoo.com>
2026-04-16 17:37:44 -0400

    Consistent column names

--- Tabula_muris/check_cell_types.py
+++ Tabula_muris/check_cell_types.py
@@ -5,7 +5,7 @@ metadata_path = "/gpfs/commons/home/nkeung/tabula_muris_data/bam_paths.csv"
 ts = pd.read_csv(metadata_path)
 
 cell_counts = ts['cell_ontology_class'].value_counts().reset_index()
-cell_counts.columns = ['cell_type', 'count']
+cell_counts.columns = ['cell_ontology_class', 'count']
 
 filtered_cells = cell_counts[(cell_counts['count'] >= 30)]
 filtered_cells = filtered_cells.sort_values(by='count', ascending=False)
