9d8da26086b57db14ec7d128f2506cae847bba2c
Nicholas Keung <nlkeung@yahoo.com>
2026-04-16 00:18:04 -0400

    Adapt for TM

--- Tabula_muris/check_cell_types.py
+++ Tabula_muris/check_cell_types.py
@@ -1,14 +1,14 @@
 import pandas as pd
 
-metadata_path = "/gpfs/commons/home/nkeung/tabula_sapiens/bam_paths.tsv"
+metadata_path = "/gpfs/commons/home/nkeung/tabula_muris_data/bam_paths.csv"
 
-ts = pd.read_csv(metadata_path, sep="\t")
+ts = pd.read_csv(metadata_path)
 
-cell_counts = ts['cell_type'].value_counts().reset_index()
+cell_counts = ts['cell_ontology_class'].value_counts().reset_index()
 cell_counts.columns = ['cell_type', 'count']
 
 filtered_cells = cell_counts[(cell_counts['count'] >= 30)]
 filtered_cells = filtered_cells.sort_values(by='count', ascending=False)
 
-filtered_cells.to_csv("/gpfs/commons/home/nkeung/tabula_sapiens/cell_counts.tsv", sep="\t", index=False)
+filtered_cells.to_csv("/gpfs/commons/home/nkeung/tabula_muris_data/cell_counts.csv", index=False)
 print(filtered_cells)
