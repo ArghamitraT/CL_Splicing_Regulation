0f1045a60ebe8699d0f7a9bea66316f3c2e65bbd
Nicholas Keung <nlkeung@yahoo.com>
2026-05-08 18:16:46 -0400

    Added adaptive weighting with MLP (FIRE)

--- ML_model/src/loss/hier_supcon.py
+++ ML_model/src/loss/hier_supcon.py
@@ -9,16 +9,42 @@ import torch.nn as nn
 import pickle
 import pandas as pd
 
+class FIREDistanceBias(nn.Module):
+    """
+    Add adaptive bias term to phylogenetic distance calculation. Based on code from
+    GPN-Star: https://www.biorxiv.org/content/10.1101/2025.09.21.677619v1
+    """
+    def __init__(self, max_dist, fire_hidden_size=32):
+        super().__init__()
+        self.c = nn.Parameter(torch.tensor(1, dtype=torch.float32))
+        self.mlp = nn.Sequential(
+            nn.Linear(1, fire_hidden_size, bias=False),
+            nn.SiLU(),
+            nn.Linear(fire_hidden_size, 1, bias=False),
+        )
+        self.max_dist = max_dist
+
+    def forward(self, dist: torch.tensor) -> torch.tensor:
+        """
+        dist: [bsz, bsz]
+        """
+        c = self.c.clamp(min=0)
+        dist_norm = torch.log(c * dist + 1) / torch.log(c * self.max_dist + 1)
+        return self.mlp(dist_norm.unsqueeze(-1)).squeeze(-1)
+
+
 class hierSupConLoss(nn.Module):
     """Supervised Contrastive Learning: https://arxiv.org/pdf/2004.11362.pdf.
     It also supports the unsupervised contrastive loss in SimCLR"""
     def __init__(self, temperature=0.07, contrast_mode='one',
-                 base_temperature=0.07, correlation_dir=None):
+                 base_temperature=0.07, correlation_dir=None, 
+                 weight_mode='fixed'):
         super(hierSupConLoss, self).__init__()
         self.temperature = temperature
         self.contrast_mode = contrast_mode
         self.base_temperature = base_temperature
         self.correlation_dir = correlation_dir
+        self.weight_mode = weight_mode
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
         
         # Load species-distance weights
@@ -38,6 +64,10 @@ class hierSupConLoss(nn.Module):
         # Save self.dist_matrix
         self.register_buffer('dist_matrix', matrix)
 
+        if self.weight_mode == "adaptive":
+            max_dist = float(matrix.max().item())
+            self.fire_bias = FIREDistanceBias(max_dist)
+
     def compute_weights(self, anchor_species, contrast_species):
         """
         Helper function to grab necessary weights from the dist_matrix tensor.
@@ -51,9 +81,18 @@ class hierSupConLoss(nn.Module):
             [self.species_to_idx[sp] for sp in contrast_species],
             device=self.dist_matrix.device
         )
-        # Select rows for all anchors at once
+        # Select rows for all anchors
         # Then all columns for the contrastive species
-        return self.dist_matrix[anchor_idx][:, contrast_idx]
+        dist = self.dist_matrix[anchor_idx[:, None], contrast_idx[None, :]]
+        
+        if self.weight_mode == "fixed":
+            return dist
+        elif self.weight_mode == "adaptive":
+            dist = self.dist_matrix[anchor_idx[:, None], contrast_idx[None, :]]
+            # Apply MLP
+            return self.fire_bias(dist)
+        else:
+            raise ValueError(f"Unknown weight mode: {self.weight_mode}")
     
     def forward(self, features, exon_name, division, species_list, labels=None, mask=None):
         """Compute loss for model. If both `labels` and `mask` are None,
