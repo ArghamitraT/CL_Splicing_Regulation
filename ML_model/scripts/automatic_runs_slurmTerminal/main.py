d259718e5f2fa8c39a1ac49e889f1006a78d6cc2
Nicholas Keung <nlkeung@yahoo.com>
2026-06-26 17:39:42 -0400

    CLADES CL settings with chromosome-split data

--- ML_model/scripts/automatic_runs_slurmTerminal/main.py
+++ ML_model/scripts/automatic_runs_slurmTerminal/main.py
@@ -14,18 +14,18 @@ def get_experiment_config():
     """Return all experiment-level parameters (edit here only)."""
 
     cfg = dict(
-        slurm_file_name = 'Psi_ASCOT_afterReview_CNN2L_testExon_yesCL',
-        task = "psi_regression_task", # "psi_regression_task" or "introns_cl"
+        slurm_file_name = 'CL_Chrom_Split',
+        task = "introns_cl", # "psi_regression_task" or "introns_cl"
         maxpooling = True,
-        max_epochs = 15,
+        max_epochs = 25,
         optimizer = "adam",
-        readme_comment = "psi finetuning running with different layeres of CNN 2Layers test exons yes to CL\n",
-        wandb_logger_NOTES = "psi finetuning running with different layeres of CNN 2Layers test exons yes to CL",
+        readme_comment = "CL chromosome data split\n",
+        wandb_logger_NOTES = "CL chromosome data split",
         new_project_wandb = 1, # only 1 for psi
         fivep_ovrhang = 300,
         threep_ovrhang = 300,
         learning_rate =  1e-3, # Best Psi, CL: 1e-3
-        global_batch_size = 1024, # Best Psi: 1024, CL: 2048
+        global_batch_size = 2048, # Best Psi: 1024, CL: 2048
         accumulate_grad_batches = 1, # Best Psi, CL: 1
         
         ##### --- machine configuration
@@ -45,9 +45,12 @@ def get_experiment_config():
         n_augmentations = 10,
         temperature     = 0.2, # Best Psi, CL: 1
     
-        TRAIN_FILE="train_merged_filtered_min30Views.pkl",
-        VAL_FILE="val_merged_filtered_min30Views.pkl",    
-        TEST_FILE="test_merged_filtered_min30Views.pkl",
+        # TRAIN_FILE="train_merged_filtered_min30Views.pkl",
+        # VAL_FILE="val_merged_filtered_min30Views.pkl",    
+        # TEST_FILE="test_merged_filtered_min30Views.pkl",
+        TRAIN_FILE="chromSplit/train_chr_merged_filtered_min30Views.pkl",
+        VAL_FILE="chromSplit/val_chr_merged_filtered_min30Views.pkl",
+        TEST_FILE="chromSplit/test_chr_merged_filtered_min30Views.pkl",
         ################### --- CL specific parameters ################### 
 
         ################### --- psi specific parameters ################### 
@@ -55,13 +58,13 @@ def get_experiment_config():
         warm_start = True,
         ascot = True, # if ASCOT dataset the true, if Tabula Sapiens false     
         psi_loss_name = "MTSpliceBCELoss",
-        # PSI_TEST_FILE = "psi_variable_Retina___Eye_psi_MERGED.pkl", # "psi_test_Retina___Eye_psi_MERGED.pkl",
-        PSI_TEST_FILE = "psi_test_Retina___Eye_psi_MERGED.pkl", # "psi_test_Retina___Eye_psi_MERGED.pkl",
+        PSI_TEST_FILE = "psi_variable_Retina___Eye_psi_MERGED.pkl", # "psi_test_Retina___Eye_psi_MERGED.pkl",
+        # PSI_TEST_FILE = "psi_test_Retina___Eye_psi_MERGED.pkl", # "psi_test_Retina___Eye_psi_MERGED.pkl",
         mtsplice_BCE = 1,
         mode = "mtsplice", # mode: or "3p", "5p", "intronOnly", "intronexon", "mtsplice"
         train_mode = "train",
         eval_weights = "exprmnt_2025_08_17__02_17_03",
-        run_num = 20,
+        run_num = 1,
         val_check_interval = 1.0,
         dropout_rate = 0.1, # Best ASCOT_Psi: 0.1, TS: 0.4
         
@@ -86,7 +89,8 @@ def get_experiment_config():
         # mtsplice_weights = "exprmnt_2026_01_28__00_43_17",  # CL swept, 300bp, no exon pad, 10 aug, check, 25 epochs
         # mtsplice_weights = "exprmnt_2026_01_29__13_00_53",  # CL swept, 300bp, no exon pad, 10 aug, CNN_6L
         # mtsplice_weights = "exprmnt_2026_01_29__13_01_25",  # CL swept, 300bp, no exon pad, 10 aug, CNN_4L
-        mtsplice_weights = "exprmnt_2026_01_29__13_01_48",  # CL swept, 300bp, no exon pad, 10 aug, CNN_2L
+        # mtsplice_weights = "exprmnt_2026_01_29__13_01_48",  # CL swept, 300bp, no exon pad, 10 aug, CNN_2L
+        mtsplice_weights = "exprmnt_2026_06_13__00_11_47",
        
         ####### ntv2 weights for 5', 3' and exon ##########
         weight_3p = "exprmnt_2025_10_20__00_59_06",
